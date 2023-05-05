466e1441bcc5d8f3936624ddd704e44cc26d65d8
Chao-Min Wu <95341874+jm-nai-rien@users.noreply.github.com>
2023-05-05 16:45:10 -0400

    Add threejs to right. change material to normal

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const recipeNameInput = document.getElementById('recipe-name');
-const searchButton = document.getElementById('search-button');
+const searchButton = document.getElementById('get-nutrition-btn');
 const resultsContainer = document.getElementById('results-container');
 
-async function searchRecipesByName(name) {
-  const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${name}`;
+let selectedRecipeId = null;
+
+async function fetchRecipes(query) {
+  const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${query}&number=10`;
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '35c93bcd3bmsh812505908d611c5p1eee90jsn7b90d749f044',
+      // 'X-RapidAPI-Key': '35c93bcd3bmsh812505908d611c5p1eee90jsn7b90d749f044',
       'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
     }
   };
@@ -21,6 +23,51 @@ async function searchRecipesByName(name) {
   }
 }
 
+// async function searchRecipesByName(name) {
+//   const results = await fetchRecipes(name);
+//   return results;
+// }
+
+//Search Dropdown Menu
+async function searchRecipesByName(name) {
+  const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${name}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '35c93bcd3bmsh812505908d611c5p1eee90jsn7b90d749f044',
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+  };
+  
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function searchRecipesById(id) {
+  const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk?ids=${id}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '35c93bcd3bmsh812505908d611c5p1eee90jsn7b90d749f044',
+      'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//Nutrition
 async function getNutritionById(id) {
   const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/nutritionWidget.json`;
   const options = {
@@ -40,6 +87,8 @@ async function getNutritionById(id) {
   }
 }
 
+
+//Ingredient
 async function getIngredientByID(id) {
   const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/ingredientWidget.json`;
   const options = {
@@ -60,18 +109,58 @@ async function getIngredientByID(id) {
 }
 
 
+function updateDropdown(recipes) {
+  const dropdown = document.getElementById('recipe-dropdown');
+  dropdown.innerHTML = '';
+
+  if (recipes.length > 0) {
+    recipes.forEach(recipe => {
+      const item = document.createElement('div');
+      item.textContent = recipe.title;
+      item.addEventListener('click', async () => {
+        recipeNameInput.value = recipe.title;
+        selectedRecipeId = recipe.id;
+        // const nutrition = await getNutritionById(recipe.id);
+        // const ingredients = await getIngredientByID(recipe.id);
+        // displayResult(recipe, nutrition, ingredients);
+        // Clear the dropdown and hide it
+        dropdown.innerHTML = '';
+        dropdown.classList.add('hidden');
+      });
+      dropdown.appendChild(item);
+    });
+    dropdown.classList.remove('hidden');
+  } else {
+    dropdown.classList.add('hidden');
+  }
+}
+
+
+
+recipeNameInput.addEventListener('input', async () => {
+  const query = recipeNameInput.value;
+  if (query.length > 2) {
+    const searchResults = await searchRecipesByName(query);
+    updateDropdown(searchResults);
+  } else {
+    const dropdown = document.getElementById('recipe-dropdown');
+    dropdown.classList.add('hidden');
+  }
+});
+
+
+
+
 searchButton.addEventListener('click', () => {
   (async () => {
-    const recipeName = recipeNameInput.value;
-  const searchResults = await searchRecipesByName(recipeName);
-
-  if (searchResults.length > 0) {
-    const firstRecipe = searchResults[0];
-    const nutrition = await getNutritionById(firstRecipe.id);
-    const ingredient = await getIngredientByID(firstRecipe.id);
-    displayResult(firstRecipe, nutrition, ingredient);
-  } else {
-    resultsContainer.innerHTML = '<p>No results found.</p>';}
+    if (selectedRecipeId) {
+      const nutrition = await getNutritionById(selectedRecipeId);
+      const ingredient = await getIngredientByID(selectedRecipeId);
+      const recipe = (await searchRecipesById(selectedRecipeId))[0];
+      displayResult(recipe, nutrition, ingredient);
+    } else {
+      resultsContainer.innerHTML = '<p>No results found.</p>';
+    }
   })();
 });
 
@@ -83,64 +172,61 @@ function displayResult(recipe, nutrition, ingredients) {
     <p>Fat: ${nutrition.fat}</p>
     <p>Carbohydrates: ${nutrition.carbs}</p>
     <p>Protein: ${nutrition.protein}</p>
+    <h3>Ingredients</h3>
     <ul>
     ${ingredients.map(ingredient => `<li>${ingredient.name}</li>`).join('')}
     </ul>
   `;
 
   // Prepare the data
-  const data = [
-    {name: 'Calories', value: parseFloat(nutrition.calories)},
-    {name: 'Fat', value: parseFloat(nutrition.fat)},
-    {name: 'Carbohydrates', value: parseFloat(nutrition.carbs)},
-    {name: 'Protein', value: parseFloat(nutrition.protein)}
-  ];
+const data = [
+  {name: 'Calories', value: parseFloat(nutrition.calories)},
+  {name: 'Fat', value: parseFloat(nutrition.fat)},
+  {name: 'Carbohydrates', value: parseFloat(nutrition.carbs)},
+  {name: 'Protein', value: parseFloat(nutrition.protein)}
+];
 
-  // Create the bar chart
-  const svg = d3.select('#nutrition-chart');
-  const width = parseInt(svg.attr('width'));
-  const height = parseInt(svg.attr('height'));
-  const margin = {top: 20, right: 20, bottom: 30, left: 40};
+// Create the circular packing chart
+const svg = d3.select('#nutrition-chart');
+const width = parseInt(svg.attr('width'));
+const height = parseInt(svg.attr('height'));
 
-  svg.selectAll('*').remove(); // Clear the previous chart
+svg.selectAll('*').remove(); // Clear the previous chart
 
-  const x = d3.scaleBand() //define the x axis
-    .domain(data.map(d => d.name))
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
+// Create a root node using d3.hierarchy and .sum to calculate the sum of the values
+const root = d3.hierarchy({children: data})
+  .sum(d => d.value);
 
-  const y = d3.scaleLinear() //dedine the y axis
-    .domain([0, d3.max(data, d => d.value)]).nice()
-    .range([height - margin.bottom, margin.top]);
+// Create a pack layout with the desired size
+const pack = d3.pack()
+  .size([width, height])
+  .padding(30);
 
-    svg.append('g')
-      .selectAll('circle')
-      .data(data)
-      .join('circle')
-      .attr('cx', d => x(d.name) + x.bandwidth() / 2)
-      .attr('cy', d => y(d.value))
-      .attr('r', d => Math.sqrt(d.value))
-      .attr('stroke', 'black')
-      .attr('fill', '#69a3b2');
-  
-      // Draw the bars
-  // svg.append('g')
-  //   .selectAll('rect')
-  //   .data(data)
-  //   .join('rect')
-  //   .attr('x', d => x(d.name))
-  //   .attr('y', d => y(d.value))
-  //   .attr('height', d => y(0) - y(d.value)) //Sets the 'height' attribute of each rectangle element based on the value property of each data item. 
-  //   .attr('width', x.bandwidth())
-  //   .attr('fill', 'steelblue');
+// Apply the pack layout to the root node
+const nodes = pack(root).descendants();
 
-  // Add x-axis
-  svg.append('g')
-    .attr('transform', `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x));
+// Create a color scale
+const color = d3.scaleOrdinal()
+  .domain(data.map(d => d.name))
+  .range(d3.schemeCategory10);
 
-  // Add y-axis
-  svg.append('g')
-    .attr('transform', `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y));
+// Draw the circles
+svg.selectAll('circle')
+  .data(nodes)
+  .join('circle')
+  .attr('cx', d => d.x)
+  .attr('cy', d => d.y)
+  .attr('r', d => d.r)
+  .attr('fill', d => d.data.name ? color(d.data.name) : 'none')
+  .attr('stroke', d => d.children ? 'none' : 'black');
+
+// Add text labels
+svg.selectAll('text')
+  .data(nodes.filter(d => !d.children))
+  .join('text')
+  .attr('x', d => d.x)
+  .attr('y', d => d.y)
+  .attr('text-anchor', 'middle')
+  .attr('dominant-baseline', 'central')
+  .text(d => d.data.name);
 }
---
+++ three.js
@@ -0,0 +1,122 @@
+import * as THREE from 'three';
+import {FontLoader} from 'three/addons/loaders/FontLoader.js';
+import {TextGeometry} from 'three/addons/geometries/TextGeometry.js';
+import {OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xFD7FCC );
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let textMesh;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth*0.5, window.innerHeight*0.5 );
+
+
+const container = document.querySelector('.threejswork');
+container.appendChild(renderer.domElement);
+
+
+//Add light source
+const dirLight = new THREE.DirectionalLight( 'white', 3 );//strength
+dirLight.position.set( 0, 30, 0 ).normalize();
+scene.add( dirLight );
+
+const ambLight = new THREE.AmbientLight( 'white', 3 );
+ambLight.color.setHSL( 23, 1, 0.5 );
+ambLight.position.set( 1, 30, 0 );
+scene.add( ambLight );
+
+//<!------------ PLANE----------------->
+// Create a large plane
+const planeGeometry = new THREE.PlaneGeometry(100, 100);
+
+// Create gradient canvas texture
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 10;
+canvas.height = 100;
+
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+const maxRadius = Math.sqrt(centerX * centerX + centerY * centerY);
+
+const gradient = ctx.createRadialGradient(centerX, centerY, 0, centerX, centerY, maxRadius);
+gradient.addColorStop(0, "#FD935A");
+gradient.addColorStop(0.5, "#FD7FCC");
+
+ctx.fillStyle = gradient;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const texture = new THREE.CanvasTexture(canvas);
+// Create the gradient material
+const gradientMaterial = new THREE.MeshBasicMaterial({ map: texture, });
+
+// Create the mesh and add it to the scene
+const mesh = new THREE.Mesh(planeGeometry, gradientMaterial);
+mesh.position.set(0, 0, -10); // Position the plane behind your scene objects
+scene.add(mesh);
+// const secondMesh = new THREE.Mesh(planeGeometry, gradientMaterial);
+// scene.add(secondMesh);
+//<!------------ PLANEEnd----------------->
+
+// Render loop required
+const loader = new FontLoader();
+loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+  const geometry = new TextGeometry('FOOD', {
+    font: font,
+    size: 7,
+    height: 2,
+    curveSegments: 10,
+    bevelEnabled: true,
+    bevelThickness: 0.1,
+    bevelSize: .1,
+    bevelSegments: 3,
+  });
+
+  const material = new THREE.MeshNormalMaterial({ 
+    wireframe: false, // Set to true if you want to display the wireframe
+    flatShading: true, // Set to true for flat shading, false for smooth shading
+    color: 0xc94477
+});
+
+textMesh = new THREE.Mesh(geometry, material);
+scene.add(textMesh);
+  
+    textMesh = new THREE.Mesh(geometry, material);
+  scene.add(textMesh);
+  // geometry.computeBoundingBox();
+  // geometry.boundingBox.getCenter(mesh.position).multiplyScalar(-1);
+
+
+  //add edge line
+  const edges = new THREE.EdgesGeometry(geometry);
+  const edgeMaterial = new THREE.LineBasicMaterial({  color: 0xFD935A, linewidth: 2 }); // Change the color as needed
+  const edgeLines = new THREE.LineSegments(edges, edgeMaterial);
+  scene.add(edgeLines);
+
+  camera.position.x = 15;
+  camera.position.z = 25;
+  camera.position.y = 40;
+  
+  const controls = new OrbitControls( camera, renderer.domElement );
+
+  controls.update();
+
+  const animate = function () {
+    requestAnimationFrame(animate);
+    controls.update();
+    // if (textMesh) {
+    //   textMesh.rotation.x += 0.01;
+    //   textMesh.rotation.y += 0.01;
+    //   // textMesh.add(edgeLines);
+    //   // textMesh.position.x += 0.01;
+    //   // textMesh.position.y += 0.01;
+    // }
+    
+    renderer.render(scene, camera);
+  };
+
+  animate();
+});
